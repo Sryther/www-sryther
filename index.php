f224881da65a0cd9fe1c69897f8b2a7c87f5630a
Paul Rey <paul.rey@y-nov.com>
2016-07-01 13:57:48 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,221 @@
+﻿<html>
+    <head>
+        <title>Paul Rey - Sryther.fr</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="/assets/bootflat/css/bootflat.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+        <script charset="utf-8" src="/assets/js/jquery-1.10.1.min.js"></script>
+        <script charset="utf-8" src="/assets/js/bootstrap.min.js"></script>
+        <!--[if lt IE 9]>
+            <script charset="utf-8" src="/assets/js/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body style="background-color: #f1f2f6">
+        <script charset="utf-8">
+            $(document).ready(function() {
+                $('[data-toggle="popover"]').popover();
+            });
+        </script>
+        <div class="container">
+            <center>
+                <h1>
+                    Paul Rey
+                </h1>
+                <h2>
+                    Etudiant & développeur
+                </h2>
+                <div class="row">
+                    <p>
+                        <a href="https://www.linkedin.com/profile/view?id=299871512&trk=spm_pic">
+                            <img src="/static/linkedin.png" />
+                        </a>
+                        &nbsp;
+                        <a href="https://github.com/Sryther?tab=repositories">
+                            <img src="/static/github.png" />
+                        </a>
+                        &nbsp;
+                        <a href="https://plus.google.com/104156603726179107503">
+                            <img src="/static/google-plus.png" />
+                        </a>
+                    </p>
+                </div>
+                <img
+                    id="me"
+                    data-toggle="popover"
+                    data-trigger="hover"
+                    title="A propos de moi"
+                    data-html="true"
+                    data-content="
+                    <address>
+                        <strong>Paul Rey</strong><br>
+                        214 avenue Raymond naves<br>
+                        Porte 104, Etage 1, Bâtiment C<br>
+                        31500 Toulouse<br>
+                        <abbr title='Téléphone'>Tel:</abbr> +(33) 6 10 93 34 14
+                        <a href='mailto:paul.rey@sryther.fr'>paul.rey@sryther.fr</a>
+                    </address>"
+                    class="logo img-rounded"
+                    alt="Logo Paul Rey - Rey Paul - Sryther"
+                    src="/assets/img/me.png"
+                />
+            </center>
+            <div class="timeline">
+                <dl>
+                    <dt>Septembre 2015</dt>
+                    <dd class="pos-right clearfix">
+                        <div class="circ"></div>
+                        <div class="time">14 sept</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/intent-technologies.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Développeur logiciel en alternance - Intent Technologies</h4>
+                                <p>Intégration de nouveaux objets connectés et de réseaux IoT à la plateforme HubIntent ainsi qu'à l'industrialisation des processus d'exploitation</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>Février 2015</dt>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">11 fév</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" width="48" src="/assets/img/logo-aquelleheure.png">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Webmaster - A quelle heure ?</h4>
+                                <p>Rédacteur et administrateur du site <a href="http://www.aquelleheure.info">www.aquelleheure.info</a></p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>Octobre 2014</dt>
+                    <dd class="pos-right clearfix">
+                        <div class="circ"></div>
+                        <div class="time">08 oct</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/interopsys.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Développeur stagiaire - InteropSys</h4>
+                                <p>Développeur d'applications web (suivi de temps de travail) et développement d'IteropSuite et de ses modules</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>Septembre 2014</dt>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">25 sep</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/ingesup.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">3ème année Ingésup Toulouse</h4>
+                                <p>Passage en 3ème année à l'école Ingésup, Bachelor 3, pour la préparation au Titre d'Expert en Informatique et Sytèmes d'Information.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>Juin 2014</dt>
+                    <dd class="pos-right clearfix">
+                        <div class="circ"></div>
+                        <div class="time">01 juin</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/interopsys.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Développeur stagiaire - InteropSys</h4>
+                                <p>Développeur d'applications web : refonte Argiopa.fr et création d'un gestionnaire de tâches.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>2012 - 2013</dt>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">2013</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <i class="fa fa-rocket fa-fw fa-3x events-object img-rounded"></i>
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Freelance</h4>
+                                <p>Développeur web pour plusieurs sociétés et assocations.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dd class="pos-right clearfix">
+                        <div class="circ"></div>
+                        <div class="time">sep 2013</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/ingesup.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Ingésup</h4>
+                                <p>2ème année préparatoire à Ingésup Toulouse.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">sep 2012</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <img class="events-object img-rounded" src="/assets/img/ingesup.jpg">
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Ingésup</h4>
+                                <p>1ère année préparatoire à Ingésup Toulouse.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dd class="pos-right clearfix">
+                        <div class="circ"></div>
+                        <div class="time">juil 2012</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <i class="fa fa-certificate fa-fw fa-3x events-object img-rounded"></i>
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Baccalauréat</h4>
+                                <p>Obtention du baccalauréat Scientifique spécialité Physique/Chimie.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">avr 2012</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <i class="fa fa-plane fa-fw fa-3x events-object img-rounded"></i>
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">BIA</h4>
+                                <p>Obtention du Brevet d'Initiation à l'Aviation mention assez bien.</p>
+                            </div>
+                        </div>
+                    </dd>
+                    <dt>1994</dt>
+                    <dd class="pos-left clearfix">
+                        <div class="circ"></div>
+                        <div class="time">06 oct</div>
+                        <div class="events">
+                            <div class="pull-left">
+                                <i class="fa fa-birthday-cake  fa-fw fa-3x events-object img-rounded"></i>
+                            </div>
+                            <div class="events-body">
+                                <h4 class="events-heading">Naissance</h4>
+                                <p>J'ai aujourd'hui 21 ans.</p>
+                            </div>
+                        </div>
+                    </dd>
+                </dl>
+            </div>
+            <br/><br/>
+        </div>
+    </body>
+</html>
